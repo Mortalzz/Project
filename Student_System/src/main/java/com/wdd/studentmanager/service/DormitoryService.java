a7b41a19497eddc3d6ea6e7a820d3ebad4a84949
Mortalzz <110045444+Mortalzz@users.noreply.github.com>
2024-08-22 13:56:52 +0800

    some file

--- Student_System/src/main/java/com/wdd/studentmanager/service/DormitoryService.java
+++
@@ -1,7 +0,0 @@
-package com.wdd.studentmanager.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.wdd.studentmanager.domain.S_dormit;
-
-public interface DormitoryService extends IService<S_dormit> {
-}
